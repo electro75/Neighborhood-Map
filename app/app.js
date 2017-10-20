a2ca9e5d9f25b47e2483a9321e78f878146d1c3b
Sahil Naikdhure <sanaikdhure@gmail.com>
2017-10-21 01:49:00 +0530

    new location

--- app/app.js
+++ app/app.js
@@ -3,6 +3,10 @@ var locations=[
 	name: 'Home',
 	position: {lat: 19.226557, lng: 72.971355},
 	title: 'firstMarker'
+},{
+	name: 'Whatta Waffle',
+	position: {lat: 19.258517, lng: 72.984844},
+	title: 'secondMarker'
 }];
 
 var markers=[];
@@ -10,23 +14,25 @@ var map;
 function initMap(){
 	map=new google.maps.Map(document.getElementById('map'),{
 		center:{lat: 19.2183, lng: 72.9781},
-		zoom: 15
+		zoom: 12
 	});
 	for(var i=0;i<locations.length;i++){
         var marker= new google.maps.Marker({
             map: map,
             position: locations[i].position,
-            title: locations[i].title
+            title: locations[i].title,
+             animation: google.maps.Animation.DROP
         });
         markers.push(marker);
     }
+    return;
 };
 function mapViewModel(){
+	// initMap();
 	var self=this;
 	this.places=ko.observableArray();
 	locations.forEach(function(obj){
 		self.places.push(obj.name);
-		// console.log(self.places()[0])
 	});
 	console.log(this.places()[0]);
 }
